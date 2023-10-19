6a9b57833935759f7f9d3779c975953fff785fdc
Alex Petrean <alex@padfoot.cc>
2023-10-19 12:17:42 +0300

    add yaml config file opts

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Editor string `yaml:"editor"`
+	Source struct {
+		Dirs           []string `yaml:"dirs"`
+		ProjectMarkers []string `yaml:"project_markers"`
+	} `yaml:"source"`
+}
+
+func New() (*Config, error) {
+	conf := &Config{}
+	file, err := os.Open(fmt.Sprintf("%s/.config/cdp.yaml", os.Getenv("HOME")))
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.NewDecoder(file).Decode(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}
+
+
+func (c Config) Dirs() []string {
+	homeDir := os.Getenv("HOME")
+	res := []string{}
+	for _, dir := range c.Source.Dirs {
+		res = append(res, fmt.Sprintf("%s/%s", homeDir, dir))
+	}
+
+	return res
+}
