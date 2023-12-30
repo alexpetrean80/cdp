cf60e3ef469a0648a0dbfec3d05744f4a46c55e1
Alex Petrean <alex_petrean@icloud.com>
2023-12-30 12:37:29 +0200

    cleanup some stuff

--- cmd/browser.go
+++ cmd/browser.go
@@ -1,12 +1,6 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +15,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("browser called")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(browserCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// browserCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// browserCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
