8cc4ff7eeb7ccfd10a7def9ec2416626b4920c3e
Alex Petrean <alex_petrean@icloud.com>
2023-12-30 12:36:22 +0200

    change directory in the root folder

--- cmd/root.go
+++ cmd/root.go
@@ -1,35 +1,47 @@
 package cmd
 
 import (
+	"log"
 	"os"
 
+	"github.com/alexpetrean80/cdp/lib"
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "cdp",
-// 	Short: "A brief description of your application",
-// 	Long: `A longer description that spans multiple lines and likely contains
-// examples and usage of using your application. For example:
-//
-// Cobra is a CLI library for Go that empowers applications.
-// This application is a tool to generate the needed files
-// to quickly create a Cobra application.`,
-	// TODO find a way to specify default command in config
-	// Run: func(cmd *cobra.Command, args []string) {
-	//
-	//  },
-}
+var (
+	last    bool
+	rootCmd = &cobra.Command{
+		Use: "cdp",
+		//	Short: "A brief description of your application",
+		//	Long: `A longer description that spans multiple lines and likely contains
+		//
+		// examples and usage of using your application. For example:
+		//
+		// Cobra is a CLI library for Go that empowers applications.
+		// This application is a tool to generate the needed files
+		// to quickly create a Cobra application.`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+			if err := lib.ChangeDirectory(last); err != nil {
+				log.Fatal(err.Error())
+			}
+		},
+		// TODO find a way to specify default command in config
+		// Run: func(cmd *cobra.Command, args []string) {
+		//
+		// },
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	rootCmd.PersistentFlags().BoolP("last", "l", false, "Change to the last project.")
+	rootCmd.PersistentFlags().BoolVarP(&last, "last", "l", false, "Change to the last project.")
+
 }
