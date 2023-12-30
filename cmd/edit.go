f8995188ede14ea0bf53ccfff5735c38af38d2ed
Alex Petrean <alex_petrean@icloud.com>
2023-12-30 12:36:04 +0200

    add suport for the edit command

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,12 +1,12 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/alexpetrean80/cdp/lib"
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +21,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("edit called")
+		if err := lib.SpawnEditor(); err != nil {
+			log.Fatal(err.Error())
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(editCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// editCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// editCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
