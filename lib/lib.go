12317ed6b5c34c0a433f9cb29a70b96d888b8446
Alex Petrean <alex_petrean@icloud.com>
2023-12-30 14:46:58 +0200

    change some stuff

--- lib/lib.go
+++ lib/lib.go
@@ -2,11 +2,16 @@ package lib
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"sync"
 
+	"github.com/alexpetrean80/cdp/finder"
+	"github.com/ktr0731/go-fuzzyfinder"
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+	"golang.org/x/sync/errgroup"
 )
 
 func GetFullPathOfDirs() ([]string, error) {
@@ -30,4 +35,112 @@ func ChangeDirectory(last bool) error {
 	}
 
 	return os.Chdir(projectPath)
+}
+
+func ReadLastProject() (string, error) {
+	file, err := os.Open(fmt.Sprintf("%s/.local/share/cdp_last", os.Getenv("HOME")))
+	if err != nil {
+		return "", err
+	}
+
+	proj, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	return string(proj), nil
+}
+
+func WriteLastProject(projectPath string) error {
+	file, err := os.Create(fmt.Sprintf("%s/.local/share/cdp_last", os.Getenv("HOME")))
+	if err != nil {
+		return err
+	}
+
+	_, err = file.WriteString(projectPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetProjectPath(last bool) (string, error) {
+	log.Println(last)
+	if last {
+		projectPath, err := ReadLastProject()
+		log.Println(projectPath)
+		if err == nil && projectPath != "" {
+			return projectPath, nil
+		}
+	}
+
+	ch := FindProjects()
+	projectPath, err := FzfPath(ch)
+	if err != nil {
+		return "", err
+	}
+
+	if err := WriteLastProject(projectPath); err != nil {
+		return projectPath, err
+	}
+
+	return projectPath, nil
+}
+
+func FindProjects() <-chan string {
+	g := new(errgroup.Group)
+	ch := make(chan string, 10)
+
+	dirs, err := GetFullPathOfDirs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	markers := viper.GetStringSlice("source.project_markers")
+	for _, dir := range dirs {
+		pf := finder.New(dir, markers, ch, g)
+		g.Go(func(rootDir string) func() error {
+			return func() error {
+				return pf.Find()
+			}
+		}(dir))
+	}
+
+	go func() {
+		defer close(ch)
+
+		if err := g.Wait(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return ch
+}
+
+func FzfPath(ch <-chan string) (string, error) {
+	mtx := new(sync.Mutex)
+	var projects []string
+	go func() {
+		for {
+			proj, ok := <-ch
+			if !ok {
+				break
+			}
+			mtx.Lock()
+			projects = append(projects, proj)
+			mtx.Unlock()
+		}
+	}()
+
+	projId, err := fuzzyfinder.Find(
+		&projects, func(i int) string {
+			return (projects)[i]
+		},
+		fuzzyfinder.WithHotReloadLock(mtx),
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return (projects)[projId], nil
 }
