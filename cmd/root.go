161a81a5c669afdd847820a85e3aa94f7968fe7d
Alex Petrean <alex@padfoot.cc>
2024-02-21 19:40:48 +0200

    fix: only cd when last is not used

--- cmd/root.go
+++ cmd/root.go
@@ -16,23 +16,12 @@ var (
 
 	rootCmd = &cobra.Command{
 		Use: "cdp",
-		// Short: "Select from all your projects ",
-		//	Long: `A longer description that spans multiple lines and likely contains
-		//
-		// examples and usage of using your application. For example:
-		//
-		// Cobra is a CLI library for Go that empowers applications.
-		// This application is a tool to generate the needed files
-		// to quickly create a Cobra application.`,
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			if err := lib.ChangeDirectory(last); err != nil {
-				log.Fatal(err.Error())
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if cmd.Use == "last" {
+				return nil
 			}
+			return lib.ChangeDirectory(last)
 		},
-		// TODO find a way to specify default command in config
-		// Run: func(cmd *cobra.Command, args []string) {
-		//
-		// },
 	}
 )
 
